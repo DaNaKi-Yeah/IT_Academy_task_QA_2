b1c577304fe3335cc4fb18c006823bed0902b64e
Daniyar <1498712fvsav@gmail.com>
2023-02-28 00:29:05 +0600

    add folder DataBase\SQL_Server add class AppDbContext

---
+++ Data/DataBase/SQL_Server/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Data.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DataBase.SQL_Server
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string? connectionString = config.GetConnectionString("ConnectionString");
+
+            optionsBuilder
+                .UseSqlServer(connectionString);
+        }
+    }
+}
