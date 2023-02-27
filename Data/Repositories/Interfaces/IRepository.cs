a5d4be095b69aba02ba439326d8f5d6051d8a871
Daniyar <1498712fvsav@gmail.com>
2023-02-28 02:09:00 +0600

    add interfaces IRepository

---
+++ Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public T Add(T entity); //C
+        public List<T> AddAll(IEnumerable<T> entities); //C
+        public List<T> GetAll(); //C
+        public T GetById(uint id); //R
+        public void Update(T entity); //U
+        public void Delete(T entity); //D
+    }
+}
