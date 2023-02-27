1139db294714f511845808723cc5a3b6836ce325
Daniyar <1498712fvsav@gmail.com>
2023-02-28 00:15:21 +0600

    add classes BaseEntity, Client, Order

---
+++ Data/Models/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using Data.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models.Entities
+{
+    public class Order : BaseEntity
+    {
+        public uint ClientID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+    }
+}
