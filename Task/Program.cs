22a755aeae706d5649539de6370be537306282af
Daniyar <1498712fvsav@gmail.com>
2023-02-28 21:00:01 +0600

    init obj and Start

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Data.Controllers.Consoles;
+using Data.DataBase.SQL_Server;
+using Data.Models;
+
+//
+//  я делал это тз в течение 24 часов (начал вчера вечером), многое не успел сделать, прошу меня понять и не сильно снижать баллы :)) 
+//
+
+
+AppDbContext dataBase = new AppDbContext();
+
+dataBase.Seed();
+
+ConsoleController consoleController = new ConsoleController(dataBase);
+
+consoleController.Start();
+
