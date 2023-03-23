5d4a4ffbf7319948528a2d5c87cfd2933b06f93a
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-03-23 18:02:36 +0600

    add Enums Table, TableAction

---
+++ Data/Models/Enums/TableAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models.Enums
+{
+    public enum TableAction
+    {
+        Add,
+        Edit,
+        Delete,
+        Return,
+        Exit,
+        ShowOrdersByClient
+    }
+}
