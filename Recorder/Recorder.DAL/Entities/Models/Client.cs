7f106a343b196eb38b152b42e49e9fa27a5c9054
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-02 00:17:57 +0600

    Add classes/models BaseEntity, Client, Order

---
+++ Recorder/Recorder.DAL/Entities/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using Recorder.DAL.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.DAL.Entities.Models
+{
+    public class Client : BaseEntity
+    {
+        public DateTime DateAdd { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string PhoneNumber { get; set; }
+        public virtual IEnumerable<Order> Orders{ get; set; }
+        public int OrderAmount => Orders.Count();
+    }
+}
---
+++ Recorder/Recorder.DAL/Entities/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using Recorder.DAL.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.DAL.Entities.Models
+{
+    public class Order : BaseEntity
+    {
+        public int ClientID { get; set; }
+        public virtual Client Client { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
