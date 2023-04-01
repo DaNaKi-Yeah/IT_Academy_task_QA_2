fa8155d6b3b744ca6e9af7d07ad1f3ab77e49f70
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-02 01:15:20 +0600

    add interface IRepository<T>, class Repository<T>

---
+++ Recorder/Recorder.DAL/Repositories/Implementations/Repository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recorder.DAL.DataBase.SQL;
+using Recorder.DAL.Entities.Common;
+using Recorder.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.DAL.Repositories.Implementations
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly DbSet<T> _dbSet;
+        
+        public Repository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _dbSet = appDbContext.Set<T>();
+        }
+
+        public int Add(T entity)
+        {
+            int id = _dbSet.Add(entity).Entity.ID;
+            _appDbContext.SaveChanges();
+
+            return id;
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _dbSet.AddRange(entities);
+            _appDbContext.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.FirstOrDefault(x => x.ID == id);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
---
+++ Recorder/Recorder.DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.DAL.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public int Add(T entity);
+        public void AddRange(IEnumerable<T> entities);
+        public T GetById(int id);
+        public IEnumerable<T> GetAll();
+        public void Update(T entity);
+        public void Remove(T entity);
+    }
+}
