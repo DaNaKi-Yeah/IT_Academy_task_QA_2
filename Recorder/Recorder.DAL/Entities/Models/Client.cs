9e4c6e13c8e328503a68cb2fff1fdcdf7d61effb
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-02 22:54:29 +0600

    add ctor Client(firstName, secondName, phoneNumber)

--- Recorder/Recorder.DAL/Entities/Models/Client.cs
+++ Recorder/Recorder.DAL/Entities/Models/Client.cs
@@ -9,11 +9,22 @@ namespace Recorder.DAL.Entities.Models
 {
     public class Client : BaseEntity
     {
+        public Client()
+        {
+
+        }
+        public Client(string firstName, string secondName, string phoneNumber)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            PhoneNumber = phoneNumber;
+        }
+
         public DateTime DateAdd { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public string PhoneNumber { get; set; }
-        public virtual IEnumerable<Order> Orders{ get; set; }
+        public virtual IEnumerable<Order> Orders { get; set; }
         public int OrderAmount => Orders is null ? 0 : Orders.Count();
     }
 }
