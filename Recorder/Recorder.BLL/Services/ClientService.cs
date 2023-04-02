b54848857c4379f33f9dfb117eb822704262128d
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-02 21:27:39 +0600

    Add folder Services with classes ClientService, OrderService

---
+++ Recorder/Recorder.BLL/Services/ClientService.cs
@@ -0,0 +1,50 @@
+﻿using Recorder.DAL.Entities.Models;
+using Recorder.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.BLL.Services
+{
+    public class ClientService
+    {
+        private readonly IRepository<Client> _clientRepository;
+
+        public ClientService(IRepository<Client> clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public Client Add(Client client)
+        {
+            return _clientRepository.Add(client);
+        }
+
+        public void AddRange(IEnumerable<Client> clients)
+        {
+            _clientRepository.AddRange(clients);
+        }
+
+        public Client GetById(int id)
+        {
+            return _clientRepository.GetById(id);
+        }
+
+        public IEnumerable<Client> GetAll()
+        {
+            return _clientRepository.GetAll();
+        }
+
+        public void Update(Client client)
+        {
+            _clientRepository.Update(client);
+        }
+
+        public void Remove(Client client)
+        {
+            _clientRepository.Remove(client);
+        }
+    }
+}
---
+++ Recorder/Recorder.BLL/Services/OrderService.cs
@@ -0,0 +1,50 @@
+﻿using Recorder.DAL.Entities.Models;
+using Recorder.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.BLL.Services
+{
+    public class OrderService
+    {
+        private readonly IRepository<Order> _orderRepository;
+
+        public OrderService(IRepository<Order> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public Order Add(Order Order)
+        {
+            return _orderRepository.Add(Order);
+        }
+
+        public void AddRange(IEnumerable<Order> orders)
+        {
+            _orderRepository.AddRange(orders);
+        }
+
+        public Order GetById(int id)
+        {
+            return _orderRepository.GetById(id);
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            return _orderRepository.GetAll();
+        }
+
+        public void Update(Order order)
+        {
+            _orderRepository.Update(order);
+        }
+
+        public void Remove(Order order)
+        {
+            _orderRepository.Remove(order);
+        }
+    }
+}
