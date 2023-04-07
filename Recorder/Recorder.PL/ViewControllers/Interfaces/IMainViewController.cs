efeb7f6dc72e2e6d44792a4266ef371644bf58af
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-07 16:24:32 +0600

    add interface IMainViewController

---
+++ Recorder/Recorder.PL/ViewControllers/Interfaces/IMainViewController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.PL.ViewControllers.Interfaces
+{
+    public interface IMainViewController
+    {
+        void Display();
+    }
+}
