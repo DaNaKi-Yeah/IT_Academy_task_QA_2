017b07bd5b4f1c51fe7693ab2e93bc011d37533a
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-07 16:09:25 +0600

    set maxLength(20) for FisrtName, SecondName in class Client

--- Recorder/Recorder.DAL/Entities/Models/Client.cs
+++ Recorder/Recorder.DAL/Entities/Models/Client.cs
@@ -1,6 +1,7 @@
 ﻿using Recorder.DAL.Entities.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,9 @@ namespace Recorder.DAL.Entities.Models
         }
 
         public DateTime DateAdd { get; set; }
+        [MaxLength(20)]
         public string FirstName { get; set; }
+        [MaxLength(20)]
         public string SecondName { get; set; }
         public string PhoneNumber { get; set; }
         public virtual IEnumerable<Order> Orders { get; set; }
