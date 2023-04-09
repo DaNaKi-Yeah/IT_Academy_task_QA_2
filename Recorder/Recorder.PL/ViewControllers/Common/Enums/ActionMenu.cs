c924f9fa59d812de302d365d78933162d33469a7
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-10 02:17:21 +0600

    add enums ActionMenu, RecorderTable

---
+++ Recorder/Recorder.PL/ViewControllers/Common/Enums/ActionMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.PL.ViewControllers.Common.Enums
+{
+    public enum ActionMenu
+    {
+        Add,
+        Edit,
+        Remove,
+        ShowClientOrders,
+        Return,
+        Exit
+    }
+}
