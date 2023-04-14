27d099e7a9ef83b59cf33699f64c3ea5395ae972
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-14 18:23:13 +0600

    add Nuget AutoMapper, class MapProfiler

---
+++ Recorder/Recorder.BLL/Helpers/MapProfiler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Recorder.DAL.Entities.Models;
+using Recorder.DTOs.OrderDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.BLL.Helpers
+{
+    public class MapProfiler : Profile
+    {
+        public MapProfiler()
+        {
+            CreateMap<Order, OrderDTO>().ReverseMap();
+            CreateMap<Order, AddOrderDTO>().ReverseMap();
+            CreateMap<Order, GetOrderDTO>().ForMember(
+                getOrderDTO => getOrderDTO.ClientName, opt => opt.MapFrom(c => $"{c.Client.FirstName} {c.Client.SecondName}"));
+        }
+    }
+}
