13f418d331f17ca372eae2810817bbee8763c825
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-14 18:22:11 +0600

    add DTOs OrderDTOs

---
+++ Recorder/Recorder.DTOs/OrderDTOs/GetOrderDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recorder.DTOs.OrderDTOs
+{
+    public class GetOrderDTO
+    {
+        public int Id { get; set; }
+        public int ClientID { get; set; }
+        public string ClientName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
